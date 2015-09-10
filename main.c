aa41b33ac563c3db69fc14fb300117e7490c0921
heylermontoya <heylers03@gmail.com>
2015-09-10 08:34:15 -0500

    se añadieron algunos comentarios en el main

--- main.c
+++ main.c
@@ -22,18 +22,16 @@ int main(void)
 	
 	start_color();	/* Permite manejar colores */
 	
-	init_pair(1, COLOR_WHITE, COLOR_BLACK);	/* Pair 1 -> Texto verde
-											   fondo Negro */	
-	
-	init_pair(2,COLOR_RED, COLOR_BLACK);
-	init_pair(3,COLOR_GREEN, COLOR_BLACK);
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);	/* Pair 1 -> Texto blanco fondo Negro */	
+	init_pair(2,COLOR_RED, COLOR_BLACK); /*Pair 2 -> Texto rojo fondo Negro*/
+	init_pair(3,COLOR_GREEN, COLOR_BLACK);/*Pair 3 -> texto verde fondo negro*/
 	
 	for(i=0; i<5; i++)
 	{
 		border( ACS_VLINE, ACS_VLINE, 
 				ACS_HLINE, ACS_HLINE, 
 				ACS_ULCORNER, ACS_URCORNER,
-				ACS_LLCORNER, ACS_LRCORNER	);
+				ACS_LLCORNER, ACS_LRCORNER	); /*Dibuja un borde en la ventana de comandos*/
 		
 		attron(COLOR_PAIR(1));	/* Activa el color verde para el 
 								   texto y negro para el fondo Pair 1*/
